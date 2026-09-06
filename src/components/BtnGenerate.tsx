a95550a96c38734cd889c06afb2874333437c041
wilson <wilson.ochoau@gmail.com>
2026-09-06 11:34:52 -0500

    Creation of the BtnGenerate component to generate a new color palette.

---
+++ src/components/BtnGenerate.tsx
@@ -0,0 +1,23 @@
+import { ReloadIcon } from './icons/Reload'
+
+interface Props {
+  loading: boolean
+  onClick: () => void
+}
+export function BtnGenerate({ loading, onClick }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={loading}
+      className="bg-neutral-800 text-white rounded-lg py-4 px-8 font-medium cursor-pointer transition-all hover:bg-neutral-700"
+    >
+      {loading ? (
+        <div id="animationReload">
+          <ReloadIcon />
+        </div>
+      ) : (
+        'Generate'
+      )}
+    </button>
+  )
+}
