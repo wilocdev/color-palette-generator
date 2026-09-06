60f34ab8a2e76490e791328ae5c7142bde8ad884
wilson <wilson.ochoau@gmail.com>
2026-09-06 09:26:55 -0500

    Create a function to contact the API.

---
+++ src/api/generatePalette.ts
@@ -0,0 +1,39 @@
+const generationSpecifications = {
+  mode: 'transformer',
+  num_colors: 4,
+  temperature: 1.2,
+  num_results: 1,
+  adjacency: [
+    '0',
+    '65',
+    '45',
+    '35',
+    '65',
+    '0',
+    '35',
+    '65',
+    '45',
+    '35',
+    '0',
+    '35',
+    '35',
+    '65',
+    '35',
+    '0'
+  ],
+  palette: ['-', '-', '-', '-']
+}
+
+export const generatePalette = async () => {
+  const response = await fetch('https://api.huemint.com/color', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(generationSpecifications)
+  })
+  if (!response.ok) throw new Error('Ocurrio un error al llamar la api')
+  const { results } = await response.json()
+
+  return results
+}
