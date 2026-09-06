b6e3b1ddb3f94a0a1f934a245a6f405bf408b0c2
wilson <wilson.ochoau@gmail.com>
2026-09-06 11:59:27 -0500

    Removed the `react-copy-to-clipboard` component and am now using the built-in JS API.

--- src/components/ColorSwatch.tsx
+++ src/components/ColorSwatch.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { CopyIcon } from './icons/Copy'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { ReloadIcon } from './icons/Reload'
 
 interface Props {
@@ -13,8 +12,10 @@ interface Props {
 export function ColorSwatch({ hex, bg, accent, loading }: Props) {
   const [copied, setCopied] = useState(false)
 
-  const handleCopy = () => {
+  const handleCopy = async () => {
     if (loading) return
+
+    await navigator.clipboard.writeText(hex)
 
     setCopied(true)
     setTimeout(() => setCopied(false), 2000)
@@ -31,26 +32,25 @@ export function ColorSwatch({ hex, bg, accent, loading }: Props) {
       className={`shadow-lg p-4 w-70 rounded-2xl flex items-center justify-between ${bg} ${accent}`}
     >
       <p className="font-bold">{hex}</p>
-      <CopyToClipboard text={hex} onCopy={handleCopy}>
-        <button
-          disabled={copied || loading}
-          className={`cursor-pointer rounded-full p-2 transition-colors ${
-            loading || copied
-              ? 'cursor-not-allowed opacity-50'
-              : bg === 'bg-background'
-                ? 'hover:bg-accent hover:text-background'
-                : 'hover:text-accent hover:bg-background'
-          }`}
-        >
-          {copied ? (
-            <div id="animationReload">
-              <ReloadIcon />
-            </div>
-          ) : (
-            <CopyIcon />
-          )}
-        </button>
-      </CopyToClipboard>
+      <button
+        onClick={handleCopy}
+        disabled={copied || loading}
+        className={`cursor-pointer rounded-full p-2 transition-colors ${
+          loading || copied
+            ? 'cursor-not-allowed opacity-50'
+            : bg === 'bg-background'
+              ? 'hover:bg-accent hover:text-background'
+              : 'hover:text-accent hover:bg-background'
+        }`}
+      >
+        {copied ? (
+          <div id="animationReload">
+            <ReloadIcon />
+          </div>
+        ) : (
+          <CopyIcon />
+        )}
+      </button>
     </div>
   )
 }
