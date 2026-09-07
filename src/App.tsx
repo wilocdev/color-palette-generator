36ffc69a8fa5b88cae44e764a213130dea58b09d
wilson <wilson.ochoau@gmail.com>
2026-09-06 21:13:05 -0500

    Order of card components within bento on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import {
   BentoLayout,
+  CardButton,
+  CardCoffe,
+  CardMood,
   CardProcessCompleted,
+  CardProfile,
   CardShowIcons,
+  CardShowWeather,
+  CardTimer,
+  CardWatchCasio,
   ColorUsage,
   Hero
 } from './components'
@@ -11,12 +18,19 @@ function App() {
   const { palette, loading, generate, paletteStyles } = usePaletteGenerator()
 
   return (
-    <div style={paletteStyles} className="p-4">
+    <div style={paletteStyles} className="p-4 mb-6">
       <Hero palette={palette} loading={loading} generate={generate} />
       <BentoLayout>
         <ColorUsage />
         <CardShowIcons />
         <CardProcessCompleted />
+        <CardButton />
+        <CardShowWeather />
+        <CardWatchCasio />
+        <CardTimer />
+        <CardMood />
+        <CardProfile />
+        <CardCoffe />
       </BentoLayout>
     </div>
   )
