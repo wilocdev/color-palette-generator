e5ed3993f70d3718f52aa9c85632758f2c87f919
wilson <wilson.ochoau@gmail.com>
2026-09-09 19:53:32 -0500

    add music control icons: Play, Pause, SkipBack, SkipForward, and MusicNote

---
+++ src/components/icons/MusicNote.tsx
@@ -0,0 +1,21 @@
+export function MusicNoteIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-music"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 17a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+      <path d="M13 17a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+      <path d="M9 17v-13l10 -2v15" />
+    </svg>
+  )
+}
---
+++ src/components/icons/Pause.tsx
@@ -0,0 +1,20 @@
+export function PauseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-player-pause"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M6 5h4v14h-4z" />
+      <path d="M14 5h4v14h-4z" />
+    </svg>
+  )
+}
